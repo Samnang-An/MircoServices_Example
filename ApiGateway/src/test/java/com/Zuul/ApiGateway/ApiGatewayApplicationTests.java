ad28455951b26ae0fbcbbe24ee3b1a1a48a2e925
samnang-an <samnang.an7@gmail.com>
2024-01-26 15:31:52 -0600

    feat: add api gateway

---
+++ ApiGateway/src/test/java/com/Zuul/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zuul.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
