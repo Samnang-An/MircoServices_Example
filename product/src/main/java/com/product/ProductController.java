aacc34ce2dc32a3b40d5c746a615526789867dd0
samnang-an <samnang.an7@gmail.com>
2024-01-28 00:57:37 -0600

    refactor: reformat code

--- product/src/main/java/com/product/ProductController.java
+++ product/src/main/java/com/product/ProductController.java
@@ -3,10 +3,8 @@ package com.product;
 import com.product.model.Product;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,14 +22,16 @@ public class ProductController {
     log.info("Payment received.");
     return product + " With StockInfo" + stockFeignClient.getStock(id);
   }
+
   @RequestMapping("/")
   public String home() {
     return "Welcome to Product";
   }
 
   @FeignClient("stock-service")
-  @RibbonClient(name="stock-service")
+  @RibbonClient(name = "stock-service")
   interface StockFeignClient {
+
     @RequestMapping("/stock/product/{id}")
     String getStock(@PathVariable("id") int id);
   }
